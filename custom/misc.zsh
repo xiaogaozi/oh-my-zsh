d71c612da62555e5f2432760312732a779b4e6d7
xiaogaozi <gaochangjian@gmail.com>
2013-04-15 12:17:53 +0800

    Add lesspipe config

--- custom/misc.zsh
+++ custom/misc.zsh
@@ -1,5 +1,9 @@
 export EDITOR='vim -f'
 export PHABRICATOR_ENV=custom/myconfig
+
 export PATH=$(brew --prefix)/bin:$(brew --prefix)/sbin:$(brew --prefix)/share/python:$(brew --prefix)/share/npm/bin:$(rbenv prefix)/bin:$HOME/bin:$PATH
 defaults write $HOME/.MacOSX/environment PATH "$PATH"
+
 fpath=($(brew --prefix)/share/zsh-completions $fpath)
+
+LESSOPEN="|lesspipe.sh %s"; export LESSOPEN
